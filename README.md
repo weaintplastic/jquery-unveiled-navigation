da71bd980f57c60ab08fd8f67caa296626bff5a5
Roland Lösslein <r.loesslein@agentur-loop.com>
2015-01-17 18:07:42 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# jquery-unveiled-navigation
+# Unveiled Navigation
+
+This plugin helps you to create sticky unveiled navigations for you website project. Read more about my intentions here: 
+
+### Features
+
+- Hides on scroll down
+- Unveils on scroll up
+- Unveils when reaching the very end of a page
+- Special state when on the very top of the page
+
+### Install
+
+- [Download the latest release](https://github.com/weaintplastic/jquery-unveiled-navigation/archive/master.zip).
+- Clone the repo: `git clone https://github.com/weaintplastic/jquery-unveiled-navigation.git`.
+- Install with [Bower](http://bower.io): `bower install jquery-unveiled-navigation`.
+
+
+### Usage
+
+    <header class="unveiled-navigation"></header>
+
+    $('.unveiled-navigation').unveiledNavigation();
+
+### Properties
+
+    offset:         0,          // Offset in px for unveiling
+    acceleration:   1,          // Acceleration of unveiling/hiding
+    topClass:       'at-top',   // Class that will be added when at the top
+    timeout:        300,        // Timeout before the navigation gets unveiled on scroll stop
+    easing:         'swing',    // Esing function that is used for automatical unveiling/hiding
+    duration:       500         // Duration of the automatic unveiling/hiding
+
+### API
+
+You can use the following functions 
+
+    $('.unveiled-navigation').data('unveiledNavigation').functionName()
+
+
+    unveil()        // Call this funciton to unveil the navigation
+    cover()         // Call this function to cover the navigation
+
+
+### Support, Bugs and Feature Requests
+Please [open an issue](https://github.com/weaintplastic/jquery-unveiled-navigation/issues/new) for questions and concerns.
+
+### Licencing
+Copyright (c) 2015 Roland Lösslein
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ dist/js/jquery.unveiledNavigation.js
@@ -0,0 +1,149 @@
+;(function($){
+
+    $.unveiledNavigation = function(element, options) {
+
+        var that        = this;
+        // MERGE OPTIONS
+        var opts        = $.extend({}, $.fn.unveiledNavigation.defaults, options);
+
+        // VIEW COMPONENTS
+        var $this       = $(element);
+
+        // PROPERTIES
+        var scrollTop               = $(window).scrollTop();
+        var scrollDirection         = 1;
+        var top                     = 0;
+        var unveilTimeout           = undefined;
+
+
+        /********************************/
+        /* PRIVATE FUNCTIONS            */
+        /********************************/
+
+        var init = function(){
+            initListeners();
+        };
+
+        var initListeners = function(){
+            $(window).on('scroll', onWindowScroll);
+            onWindowScroll();
+        };
+
+
+        /**
+         * unveil
+         * Unveils the navigation
+         **/
+        this.unveil = function(){
+            $this.animate(
+                {
+                    top: 0
+                },{
+                    step: function( now ){
+                        top = now;
+                    },
+                    easing: opts.easing,
+                    duration: opts.duration
+                }
+            );
+        };
+
+        /**
+         * cover
+         * covers the navigation
+         **/
+        this.cover = function(){
+            $this.animate(
+                {
+                    top: -$this.outerHeight()
+                },{
+                    step: function( now ){
+                        top = now;
+                    },
+                    easing: opts.easing,
+                    duration: opts.duration
+                }
+            );
+        }
+
+        /********************************/
+        /* EVENT HANDLING               */
+        /********************************/
+
+        var onWindowScroll = function( event ){
+
+            // Stop unveil animation if any
+            $this.stop();
+
+            // Calculate new scrollTop and scrollDelta
+            var newScrollTop       = Math.max(0, $(window).scrollTop());
+            var scrollDelta        = scrollTop - newScrollTop;
+
+            // Calculate new top position
+            var newTop          = top + scrollDelta * opts.acceleration;
+            newTop              = Math.max(newTop, -$this.outerHeight() - opts.offset);
+            newTop              = Math.min(newTop, 0);
+
+            // Unveil completely if just partly unveiled
+            clearTimeout(unveilTimeout);
+            if(scrollDirection == 1 && Math.abs(newTop) < $this.outerHeight()){
+                unveilTimeout = setTimeout(that.unveil, opts.timeout)
+            }
+
+            // Show when scrolled to the very bottom
+            if(newScrollTop == $(document).height() - $(window).height()){
+                that.unveil();
+            }
+
+            // Assign class if scrolled to the very top
+            if(newScrollTop == 0){
+                $this.addClass(opts.topClass);
+            }else if(newScrollTop > $this.outerHeight()){
+                $this.removeClass(opts.topClass);
+            }
+
+            // Asign new position
+            $this.css({ top: newTop });
+
+
+            // Update memory
+            top                = newTop;
+            scrollTop          = newScrollTop;
+            scrollDirection    = scrollDelta ? scrollDelta/Math.abs(scrollDelta) : scrollDirection;
+        };
+
+        /********************************/
+        /* INIT                         */
+        /********************************/
+
+        init();
+
+    };
+
+    /**
+    * PLUGIN INIT
+    *
+    */
+    $.fn.unveiledNavigation = function(options){
+        return this.each(function() {
+            if (undefined === $(this).data('unveiledNavigation')) {
+                var plugin = new $.unveiledNavigation(this, options);
+                $(this).data('unveiledNavigation', plugin);
+            }
+        });
+    };
+
+    /**
+    * PLUGIN DEFAULTS
+    *
+    */
+    $.fn.unveiledNavigation.defaults = {
+        offset:         0,          // Offset in px for unveiling
+        acceleration:   1,          // Acceleration of unveiling/hiding
+        topClass:       'at-top',   // Class that will be added when at the top
+        timeout:        300,        // Timeout before the navigation gets unveiled on scroll stop
+        easing:         'swing',    // Esing function that is used for automatical unveiling/hiding
+        duration:       500         // Duration of the automatic unveiling/hiding
+    };
+
+})(jQuery);
